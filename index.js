883b6c5c9122a0a4f2e12b5fa12fc297142ff985
Christvinod <vk637312@gmail.com>
2024-03-22 17:44:57 +0530

    Animation

---
+++ index.js
@@ -0,0 +1,51 @@
+
+var crsr = document.querySelector("#cursor")
+var blur = document.querySelector("#cursor-blur")
+document.addEventListener("mousemove",function(dets){
+       crsr.style.left = dets.x+"px"
+       crsr.style.top = dets.y+"px"
+       blur.style.left = dets.x -250 +"px"
+       blur.style.top = dets.y - 250 +"px"
+   
+
+})
+
+document.addEventListener("mousemove",function(dets){
+    // blur.style.left = dets.x+"px"
+    // crsr.style.top = dets.y+"px"
+
+})
+
+
+
+
+
+
+
+gsap.to("#nav",{
+     backgroundColor : "#000",
+     durration:0.5,
+     height:"110px",
+     scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        // markers:true,
+        start:"top -10%",
+        end:"top -11%",
+        scrub:1,
+
+     }
+})
+
+gsap.to("#main",{
+    backgroundColor : "#000",
+    scrollTrigger:{
+        trigger:"body",
+        scroller:"body",
+        markers:true,
+        start:"top -25%",
+        end: "top -70%",
+        scrub: "2",
+
+    }
+})
